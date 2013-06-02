9b0b793b9a3b0510ce29c4efca4a8b76bd91f270
bgetsug <bgetsug@ibsys.com>
2013-06-02 01:42:32 -0500

    cleaning up formatting

--- src/Shift31/HAProxyClient.php
+++ src/Shift31/HAProxyClient.php
@@ -2,7 +2,7 @@
 
 namespace Shift31;
 
-use Zend\Http\Client;
+use Zend\Http\Client, Zend\Http\Request;
 
 /**
  * Class HAProxyClient
@@ -12,75 +12,78 @@ use Zend\Http\Client;
 class HAProxyClient
 {
 
-	const STATUS_DOWN = 'DOWN';
-	const STATUS_UP = 'UP';
-	const ACTION_DISABLE = 'disable';
-	const ACTION_ENABLE = 'enable';
+    const STATUS_DOWN = 'DOWN';
+    const STATUS_UP = 'UP';
+    const ACTION_DISABLE = 'disable';
+    const ACTION_ENABLE = 'enable';
 
-	protected $_checkMaxRetries;
+    protected $_checkMaxRetries;
 
     protected $_port;
     protected $_baseUrl;
     protected $_username;
     protected $_password;
-	protected $_serverFqdn;
+    protected $_serverFqdn;
     protected $_statsUrl;
-	protected $_client;
-	protected $_logger;
+    protected $_client;
+    protected $_logger;
 
 
-	/**
-	 * @param string|null	$serverFqdn
-	 * @param int		$port
-	 * @param string	$baseUrl
-	 * @param string	$username
-	 * @param string	$password
-	 * @param int		$checkMaxRetries
-	 * @param null		$logger
-	 */
-	public function __construct($serverFqdn, $port, $baseUrl, $username, $password, $checkMaxRetries = 15, $logger = null)
+    /**
+     * @param string|null $serverFqdn
+     * @param int         $port
+     * @param string      $baseUrl
+     * @param string      $username
+     * @param string      $password
+     * @param int         $checkMaxRetries
+     * @param null        $logger
+     */
+    public function __construct(
+        $serverFqdn, $port, $baseUrl, $username, $password, $checkMaxRetries = 15, $logger = null
+    )
     {
-		$this->_serverFqdn = $serverFqdn;
+        $this->_serverFqdn = $serverFqdn;
         $this->_port = $port;
         $this->_baseUrl = $baseUrl;
         $this->_username = $username;
         $this->_password = $password;
-		$this->_checkMaxRetries = $checkMaxRetries;
-		$this->_logger = $logger;
+        $this->_checkMaxRetries = $checkMaxRetries;
+        $this->_logger = $logger;
 
-		$this->_client = new Client();
-		$this->_client->setAuth($this->_username, $this->_password);
+        $this->_client = new Client();
+        $this->_client->setAuth($this->_username, $this->_password);
 
-		if ($serverFqdn != null) {
-			$this->setStatsUrl($serverFqdn);
-			$this->setClientUri($serverFqdn);
-		}
+        if ($serverFqdn != null) {
+            $this->setStatsUrl($serverFqdn);
+            $this->setClientUri($serverFqdn);
+        }
     }
 
-    
+
     /**
      * @param string $serverFqdn
      */
     public function setStatsUrl($serverFqdn)
     {
-		$this->_serverFqdn = $serverFqdn;
-        $this->_statsUrl = "http://{$this->_username}:{$this->_password}@$serverFqdn:{$this->_port}{$this->_baseUrl};csv";
+        $this->_serverFqdn = $serverFqdn;
+        $this->_statsUrl
+            = "http://{$this->_username}:{$this->_password}@$serverFqdn:{$this->_port}{$this->_baseUrl};csv";
     }
 
 
-	/**
-	 * @param $serverFqdn
-	 */
-	public function setClientUri($serverFqdn)
-	{
-		$this->_serverFqdn = $serverFqdn;
-		$this->_client->setUri("http://$serverFqdn:{$this->_port}{$this->_baseUrl}");
-	}
-    
-    
+    /**
+     * @param $serverFqdn
+     */
+    public function setClientUri($serverFqdn)
+    {
+        $this->_serverFqdn = $serverFqdn;
+        $this->_client->setUri("http://$serverFqdn:{$this->_port}{$this->_baseUrl}");
+    }
+
+
     /**
      *
-     * @return array $stats 
+     * @return array $stats
      */
     public function getStatsByProxy()
     {
@@ -93,14 +96,14 @@ class HAProxyClient
 
             foreach ($lines as $line) {
 
-				// skip header
+                // skip header
                 if (preg_match('/#/', $line)) {
                     continue;
                 }
 
                 $csv = str_getcsv($line);
 
-				// skip non-servers
+                // skip non-servers
                 if (preg_match('/FRONTEND|BACKEND/', $csv[1])) {
                     continue;
                 }
@@ -114,11 +117,11 @@ class HAProxyClient
         return $stats;
     }
 
-    
+
     /**
-     * @param array		$loadBalancers	an array of HAProxy server FQDNs
-	 *
-     * @return array	$stats
+     * @param array $loadBalancers    an array of HAProxy server FQDNs
+     *
+     * @return array    $stats
      */
     public function getStatsFromCluster(array $loadBalancers)
     {
@@ -142,140 +145,156 @@ class HAProxyClient
 
 
     /**
-     * @param array     $loadBalancers	an array of HAProxy server FQDNs
-     * @param string    $proxyName
-     * @param string    $serviceName
-     * @param string    $statusToCheck
+     * @param array  $loadBalancers    an array of HAProxy server FQDNs
+     * @param string $proxyName
+     * @param string $serviceName
+     * @param string $statusToCheck
      *
      * @return void
      */
     public function checkServiceStatusInCluster(array $loadBalancers, $proxyName, $serviceName, $statusToCheck = 'UP')
     {
         $statusVerified = false;
-		$noServiceInProxy = false;
-		$lbTries = 0;
+        $noServiceInProxy = false;
+        $lbTries = 0;
 
-		while ($statusVerified === false && $lbTries < $this->_checkMaxRetries) {
-			$lbTries++;
+        while ($statusVerified === false && $lbTries < $this->_checkMaxRetries) {
+            $lbTries++;
 
-			$clusterStats = $this->getStatsFromCluster($loadBalancers);
+            $clusterStats = $this->getStatsFromCluster($loadBalancers);
 
-			if (count($clusterStats) <= 0) {
-				$this->_log('alert', "Unable to retrieve stats from any of $serviceName's load balancers. Please contact a Systems Administrator.");
-				break; // break out of while loop
-			}
+            if (count($clusterStats) <= 0) {
+                $this->_log(
+                    'alert', "Unable to retrieve stats from any of $serviceName's load balancers. Please contact a Systems Administrator."
+                );
+                break; // break out of while loop
+            }
 
-			foreach ($clusterStats as $loadBalancer => $stats) {
+            foreach ($clusterStats as $loadBalancer => $stats) {
 
-				if (isset($stats[$proxyName][$serviceName])) {
+                if (isset($stats[$proxyName][$serviceName])) {
 
-					$proxyServiceStatus = $stats[$proxyName][$serviceName]['status'];
-					$this->_log('debug', "$loadBalancer: Status for service '$serviceName' in proxy '$proxyName' is '$proxyServiceStatus'");
+                    $proxyServiceStatus = $stats[$proxyName][$serviceName]['status'];
+                    $this->_log(
+                        'debug', "$loadBalancer: Status for service '$serviceName' in proxy '$proxyName' is '$proxyServiceStatus'"
+                    );
 
-					if ($statusToCheck == 'UP') {
-						$statusVerified = ($proxyServiceStatus == 'UP') ? true : false;
-					} else {
-						// make sure status is 'DOWN' or 'MAINT' and has zero current connections
-						$statusVerified = ($proxyServiceStatus == 'DOWN' || $proxyServiceStatus == 'MAINT') && ($stats[$proxyName][$serviceName]['scur'] == '0') ? true : false;
-					}
+                    if ($statusToCheck == 'UP') {
+                        $statusVerified = ($proxyServiceStatus == 'UP') ? true : false;
+                    } else {
+                        // make sure status is 'DOWN' or 'MAINT' and has zero current connections
+                        $statusVerified = ($proxyServiceStatus == 'DOWN' || $proxyServiceStatus == 'MAINT')
+                        && ($stats[$proxyName][$serviceName]['scur'] == '0') ? true : false;
+                    }
 
-				} else {
-					$this->_log('alert', "Unable to find service named '$serviceName' in proxy named '$proxyName' on $loadBalancer. Please contact a Systems Administrator.");
-					$noServiceInProxy = true;
-					break 2; // break out of while loop
-				}
-			}
+                } else {
+                    $this->_log(
+                        'alert', "Unable to find service named '$serviceName' in proxy named '$proxyName' on $loadBalancer. Please contact a Systems Administrator."
+                    );
+                    $noServiceInProxy = true;
+                    break 2; // break out of while loop
+                }
+            }
 
-			// wait 2 seconds between tries
-			sleep(2);
-		}
+            // wait 2 seconds between tries
+            sleep(2);
+        }
 
-		if ($statusVerified === true) {
-			$this->_log('info', "HAProxy detected service '$serviceName' of proxy '$proxyName' is '$statusToCheck'.  Retried $lbTries times (" . $lbTries * 2 . ' seconds).');
-		} elseif ($noServiceInProxy === false) {
-			$this->_log('warn', "HAProxy did not detect service '$serviceName' of proxy '$proxyName' as '$statusToCheck'.  Retried $lbTries times (" . $lbTries * 2 . ' seconds).');
-		}
+        if ($statusVerified === true) {
+            $this->_log(
+                'info', "HAProxy detected service '$serviceName' of proxy '$proxyName' is '$statusToCheck'.  Retried $lbTries times ("
+                . $lbTries * 2 . ' seconds).'
+            );
+        } elseif ($noServiceInProxy === false) {
+            $this->_log(
+                'warn', "HAProxy did not detect service '$serviceName' of proxy '$proxyName' as '$statusToCheck'.  Retried $lbTries times ("
+                . $lbTries * 2 . ' seconds).'
+            );
+        }
     }
 
 
-	/**
-	 * @param string	$serverName
-	 * @param string	$action
-	 * @param string	$proxyName
-	 *
-	 * @return bool
-	 */
-	public function setServerStatus($serverName, $action = 'enable', $proxyName = 'all')
-	{
-		// perform action on all proxies where server exists
-		if ($proxyName == 'all') {
-			$proxyNames = array();
-			$stats = $this->getStatsByProxy();
+    /**
+     * @param string $serverName
+     * @param string $action
+     * @param string $proxyName
+     *
+     * @return bool
+     */
+    public function setServerStatus($serverName, $action = 'enable', $proxyName = 'all')
+    {
+        // perform action on all proxies where server exists
+        if ($proxyName == 'all') {
+            $proxyNames = array();
+            $stats = $this->getStatsByProxy();
 
-			foreach ($stats as $proxyName => $serviceNames) {
-				if (array_key_exists($serverName, $serviceNames)) {
-					$proxyNames[] = $proxyName;
-				}
-			}
-		} else {
-			$proxyNames = array($proxyName);
-		}
+            foreach ($stats as $proxyName => $serviceNames) {
+                if (array_key_exists($serverName, $serviceNames)) {
+                    $proxyNames[] = $proxyName;
+                }
+            }
+        } else {
+            $proxyNames = array($proxyName);
+        }
 
-		foreach ($proxyNames as $proxyName) {
-			try {
-				$this->_client->setMethod(\Zend_Http_Client::POST);
-				$this->_client->setParameterPost('s', $serverName);
-				$this->_client->setParameterPost('b', $proxyName);
-				$this->_client->setParameterPost('action', $action);
-				$this->_client->request();
-			} catch (\Exception $e) {
-				$this->_log('crit', "Exception: " . $e->getCode() . '-' . $e->getMessage());
-				$this->_log('debug', "Exception Trace: " . $e->getTraceAsString());
-			}
+        foreach ($proxyNames as $proxyName) {
+            try {
+                $this->_client->setMethod(Request::METHOD_POST);
+                $this->_client->setParameterPost(
+                    array(
+                         's' => $serverName,
+                         'b' => $proxyName,
+                         'action', $action
+                    )
+                );
+                $this->_client->send();
+            } catch (\Exception $e) {
+                $this->_log('crit', "Exception: " . $e->getCode() . '-' . $e->getMessage());
+                $this->_log('debug', "Exception Trace: " . $e->getTraceAsString());
+            }
 
-			$response = $this->_client->getLastResponse();
-			$success = $response->isSuccessful();
+            $response = $this->_client->getResponse();
 
-			if ($success) {
-				$this->_log('info', ucwords($action) . "d '$serverName' in proxy '$proxyName' on $this->_serverFqdn");
+            if ($response->isSuccess()) {
+                $this->_log('info', ucwords($action) . "d '$serverName' in proxy '$proxyName' on $this->_serverFqdn");
 
-				// get the status notice displayed on the page
-				/** @noinspection PhpUndefinedMethodInspection */
-				$notice = str_replace(array('[X]', '.'), '', qp($response->getRawBody(), 'div')->text());
-				$this->_log('debug', "HAProxy response on $this->_serverFqdn: $notice");
-			} else {
-				$this->_log('err', "Failed to $action '$serverName' in proxy '$proxyName' on $this->_serverFqdn");
-			}
-		}
-	}
-
-
-	/**
-	 * @param array  	$loadBalancers	an array of HAProxy server FQDNs
-	 * @param string	$serverName
-	 * @param string 	$action
-	 * @param string 	$proxyName
-	 */
-	public function setServerStatusInCluster(array $loadBalancers, $serverName, $action = 'enable', $proxyName = 'all')
-	{
-		foreach ($loadBalancers as $loadBalancer) {
-			$this->setStatsUrl($loadBalancer);
-			$this->setClientUri($loadBalancer);
-			$this->setServerStatus($serverName, $action, $proxyName);
-		}
-	}
+                // get the status notice displayed on the page
+                /** @noinspection PhpUndefinedMethodInspection */
+                $notice = str_replace(array('[X]', '.'), '', qp($response->getRawBody(), 'div')->text());
+                $this->_log('debug', "HAProxy response on $this->_serverFqdn: $notice");
+            } else {
+                $this->_log('err', "Failed to $action '$serverName' in proxy '$proxyName' on $this->_serverFqdn");
+            }
+        }
+    }
 
 
-	/**
-	 *
-	 * @param string $priority
-	 * @param string $message
-	 */
-	protected function _log($priority, $message)
-	{
-		if ($this->_logger != null) {
-			$class = str_replace(__NAMESPACE__ . "\\", '', get_called_class());
-			$this->_logger->$priority("[$class] - $message");
-		}
-	}
+    /**
+     * @param array  $loadBalancers    an array of HAProxy server FQDNs
+     * @param string $serverName
+     * @param string $action
+     * @param string $proxyName
+     */
+    public function setServerStatusInCluster(array $loadBalancers, $serverName, $action = 'enable', $proxyName = 'all')
+    {
+        foreach ($loadBalancers as $loadBalancer) {
+            $this->setStatsUrl($loadBalancer);
+            $this->setClientUri($loadBalancer);
+            $this->setServerStatus($serverName, $action, $proxyName);
+        }
+    }
+
+
+    /**
+     *
+     * @param string $priority
+     * @param string $message
+     */
+    protected function _log($priority, $message)
+    {
+        if ($this->_logger != null) {
+            $class = str_replace(__NAMESPACE__ . "\\", '', get_called_class());
+            $this->_logger->$priority("[$class] - $message");
+        }
+    }
 }
